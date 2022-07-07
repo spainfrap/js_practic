1269658fceca82b33b4e4aac6f44e3e66a09844a
Pavel <fil_imon@mail.ru>
2022-07-07 14:20:49 +0300

    task 4 done

--- script.js
+++ script.js
@@ -23,78 +23,78 @@
 
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: () => {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: () => {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                  b = prompt('На сколько оцените его?', '');
+
+            if (a != '' && b != '' && a != null && b != null && a.length < 50 ) {
+                personalMovieDB.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: () => {
+        if (personalMovieDB.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            console.log('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30 ) {
+            console.log('Вы киноман');
+        } else {console.log('Произошла ошибка');}
+    },
+    showMyDB: (hidden) => {
+        if (!hidden) {
+            console.log(personalMovieDB)
+        }
+    },
+    //showMyDB: () => {
+    //     if (personalMovieDB.privat == false) {
+    //         console.log(personalMovieDB)
+    //     }
+    // }
+    toggleVisibleMyDB: () => {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
+    writeYourGenres: () => {
+        for (let i = 1; i < 4; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
+
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i - 1] = genre;
+            }
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`)
+        });
+    }
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
 
-        if (a != '' && b != '' && a != null && b != null && a.length < 50 ) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30 ) {
-        console.log('Вы киноман');
-    } else {console.log('Произошла ошибка');}
-}
-
-detectPersonalLevel();
-
-console.log(personalMovieDB);
-
-// function showMyDB() {
-//     if (personalMovieDB.privat == false) {
-//         console.log(personalMovieDB)
-//     }
-// }
-// showMyDB();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB)
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-
-function writeYourGenres() {
-    for (let i = 1; i < 4; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);;
-    }
-}
-writeYourGenres();
 /* Задание на 2 урок:
 // 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
 
@@ -121,5 +121,23 @@ false - выводит в консоль главный объект прогр�
 genres
 
 P.S. Функции вызывать не обязательно*/
+
+// Код возьмите из предыдущего домашнего задания
+
+/* Задание на 4 урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку.
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены -
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+'use strict';
 
 // Код возьмите из предыдущего домашнего задания
--- test.js
+++ test.js
@@ -1,29 +1,171 @@
+// // Место для первой задачи
+// function sayHello(name) {
+//     return("Привет, "+ name +"!")
+// }
+
+// // Место для второй задачи
+// function returnNeighboringNumbers(dig) {
+//     return [dig - 1, dig, dig + 1];
+// }
+
+// // Место для третьей задачи
+// function getMathResult(baseNum, iterator) {
+//     if (typeof(iterator) !== 'number' || iterator <= 0) {
+//         return baseNum;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= iterator; i++) {
+//         if (i === iterator) {
+//             str += `${baseNum * i}`
+//         } else {
+//             str += `${baseNum * i}---`
+//         }
+//     }
+//     return str;
+// }
+
+// console.log(getMathResult(5, 3));
+
 // Место для первой задачи
-function sayHello(name) {
-    return("Привет, "+ name +"!")
+function calculateVolumeAndArea(arg) {
+    if (typeof(arg) !== 'number' || !Number.isInteger(arg) || arg <= 0) {
+        return "При вычислении произошла ошибка";
+    } else {
+        console.log(`Объем куба: ${Math.pow(arg, 3)}, площадь всей поверхности: ${arg * arg * 6}`)
+
+    }
+}
+calculateVolumeAndArea(0);
+// Место для второй задачи
+function getCoupeNumber(num) {
+    if (typeof(num) !== 'number' || num < 0 || !Number.isInteger(num)) {
+        return console.log("Ошибка. Проверьте правильность введенного номера места");
+    }
+
+    if (num === 0 || num > 36) {
+        return console.log("Таких мест в вагоне не существует");
+    }
+
+    return console.log(Math.floor(num / 4));
+
+}
+getCoupeNumber(1,2);
+
+
+
+
+// ================
+
+
+// Место для первой задачи
+function calculateVolumeAndArea(arg) {
+    if (typeof(arg) !== 'number' || !Number.isInteger(arg) || arg < 0) {
+        return "При вычислении произошла ошибка";
+    } else {
+        return (`Объем куба: ${Math.pow(arg, 3)}, площадь всей поверхности: ${arg * arg * 6}`)
+
+    }
 }
 
 // Место для второй задачи
-function returnNeighboringNumbers(dig) {
-    return [dig - 1, dig, dig + 1];
-}
-
-// Место для третьей задачи
-function getMathResult(baseNum, iterator) {
-    if (typeof(iterator) !== 'number' || iterator <= 0) {
-        return baseNum;
+function getCoupeNumber(num) {
+if (typeof(num) !== 'number' || num < 0 || !Number.isInteger(num)) {
+        return console.log("Ошибка. Проверьте правильность введенного номера места");
     }
 
-    let str = '';
+    if (num === 0 || num > 36) {
+        return ("Таких мест в вагоне не существует");
+    }
 
-    for (let i = 1; i <= iterator; i++) {
-        if (i === iterator) {
-            str += `${baseNum * i}`
-        } else {
-            str += `${baseNum * i}---`
+    return (Math.ceil(num / 4));
+
+}
+
+
+// ==================
+function getCoupeNumber(seatNumber) {
+    if (typeof(seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+
+    if (seatNumber === 0 || seatNumber > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+
+    for (let i = 4; i <= 36; i = i + 4) {
+        if (seatNumber <= i) {
+            return Math.ceil(i / 4);
         }
     }
-    return str;
+    // тут очень много вариантов решения, но выбрал один из элегантных :)
 }
 
-console.log(getMathResult(5, 3));
+// =========================
+
+
+// Место для первой задачи
+function getTimeFromMinutes(min) {
+    if (min < 0 || min > 600 || !Number.isInteger(min) || typeof(min) !== 'number') {
+        return console.log('Ошибка, проверьте данные')
+    }
+    const hours = Math.floor(min / 60),
+        minutes = min % 60;
+
+    let hourStr = '';
+
+    switch (hours) {
+        case 0:
+            hourStr = 'часов';
+            break;
+        case 1:
+            hourStr = 'час';
+            break;
+        case 2:
+        case 3:
+        case 4:
+            hourStr = 'часа';
+            break;
+        default:
+            hourStr = 'часов';
+    }
+
+    return console.log(`Это ${hours} часов и ${minutes} минут`)
+
+}
+getTimeFromMinutes(502)
+
+
+// Место для второй задачи
+function findMaxNumber(w, x, y, z) {
+    if (typeof(w) !== 'number' || typeof(x) !== 'number' || typeof(y) !== 'number' || typeof(z) !== 'number') {
+        return 0;
+    } else {
+        return console.log(`Самое большое число: ${Math.max(w, x, y, z)}`);
+    }
+}
+findMaxNumber(1, 5, 99, 3.5)
+
+// ==============================
+
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    }
+};
+
+function showExperience(plan) {
+
+}
+
+function showProgrammingLangs(plan) {
+
+}
